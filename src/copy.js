7cb86e13451c5f3e31018be864ff2f690868908e
0x6b <0x6b@rollem.org>
2017-12-16 12:19:14 +0900

    Prevent copying unnecessary quote

--- src/copy.js
+++ src/copy.js
@@ -2,6 +2,6 @@ import { getSelectionAsMarkdown, doCopy } from './lib/util'
 
 let text      = `[${document.title}](${document.URL})`
 let selection = getSelectionAsMarkdown()
-if (selection !== '') text += `\n\n${selection}`
+if (selection !== '> ') text += `\n\n${selection}`
 
 doCopy(text)
